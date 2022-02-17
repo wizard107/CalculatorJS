82c86031f9790dd10a8a3f40295c1a3c68a1097b
athar <athar.adel@gmx.de>
2022-02-17 11:49:04 +0100

    new

---
+++ site.js
@@ -0,0 +1,19 @@
+
+
+var input = document.getElementById('input'),
+    number = document.getElementById('.numbers div'),
+    operator = document.getElementById('.operators div'),
+    clear = document.getElementById('clear'),
+    result = document.getElementById('result'),
+    resultDisplayed = false;
+
+for(var i=0;i<number.length;i++){
+    number[i].addEventListener("click", function(e){
+
+        var currentString = input.innerHTML;
+        var lastChar = currentString[currentString.length-1];
+
+
+
+    });
+}
